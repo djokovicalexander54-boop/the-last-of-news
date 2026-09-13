cc5faeb25eac3b61298ecbc6b8a4d0f3c7366b56
alexander <djokovicalexander54@gmail.com>
2026-09-13 14:04:32 +0330

    Update scrapt_news.py

--- scrapt_news.py
+++ scrapt_news.py
@@ -146,5 +146,5 @@ app = Flask(__name__)
 def home():
     asyncio.run(main())
 if __name__ == '__main__':
-    port = int(os.environ("PORT", 8080))
+    port = int(os.environ.get("PORT", 8080))
     app.run(port=port, host='0.0.0.0')
